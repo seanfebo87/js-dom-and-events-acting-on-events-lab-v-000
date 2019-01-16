7125e426b78f92ddd35ac365b527a24f4e266c50
Sean  Febo <seanfebo@techie.com>
2019-01-16 03:32:00 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,7 +0,0 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
-}
-
-preventRefreshOnSubmit()
